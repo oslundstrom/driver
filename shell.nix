8c298112777f3ee7118eba8a7f9ffc2950310000
Oskar Lundstrom <oslundstrom@gmail.com>
2024-07-03 21:07:44 +0200

    Add nix-shell file for building arm kernel

--- shell.nix
+++ shell.nix
@@ -1,7 +1,25 @@
-with (import <nixpkgs> {}) ;
-mkShell {
-  buildInputs = [
-    gcc-arm-embedded
-  ];
-}
+let
+hostPkgs = import <nixpkgs> {};
+pkgs = import <nixpkgs> {
+  crossSystem = (import <nixpkgs/lib>).systems.examples.armv7l-hf-multiplatform;
+};
+in
+    pkgs.mkShell rec {
+      depsBuildBuild = [hostPkgs.buildPackages.stdenv.cc ];
+      nativeBuildInputs = with hostPkgs.buildPackages; [
+        bison
+        flex
+        elfutils
+        bc
+        pkg-config
+        ncurses
+        openssl
+      ];
+      buildInputs = with pkgs; [ zlib ];
+      shellHook = ''
+        export CROSS_COMPILE=armv7l-unknown-linux-gnueabihf-
+        export ARCH=arm
+        export PKG_CONFIG_PATH="${hostPkgs.ncurses.dev}/lib/pkgconfig"
+      '';
+    }
 
